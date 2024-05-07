e5254a4fdbcc8385df5b8961ecca6112f32be5c4
Bastien Dumont <bastienxs@gmail.com>
2024-05-07 22:48:34 +0200

    feed follow

---
+++ handler_feed_follow.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/bastiendmt/rss-aggregator/internal/database"
+	"github.com/google/uuid"
+)
+
+func (apiCfg *apiConfig) handleCreateFeedFollow(w http.ResponseWriter, r *http.Request, user database.User) {
+	type parameters struct {
+		FeedId uuid.UUID `json:"feed_id"`
+		URL    string    `json:"url"`
+	}
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Error while parsing JSON: %s", err))
+	}
+
+	feedFollow, err := apiCfg.DB.CreateFeedFollow(r.Context(), database.CreateFeedFollowParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		UserID:    user.ID,
+		FeedID:    params.FeedId,
+	})
+
+	if err != nil {
+		respondWithError(w, 400, fmt.Sprintf("Couldn't create feed follow: %s", err))
+		return
+	}
+
+	respondWithJSON(w, 201, dataFeedFollowToFeedFollow(feedFollow))
+}
